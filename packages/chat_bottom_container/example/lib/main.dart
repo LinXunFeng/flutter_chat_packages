7a0f300a9d91c42b4c643a04f8d86cb3229388df
LinXunFeng <linxunfeng@yeah.net>
2024-06-25 22:44:35 +0800

    update(chat_bottom_container): example

--- packages/chat_bottom_container/example/lib/main.dart
+++ packages/chat_bottom_container/example/lib/main.dart
@@ -39,8 +39,7 @@ class _HomeState extends State<Home> {
 
   final FocusNode inputFocusNode = FocusNode();
 
-  ChatBottomPanelContainerController<PanelType> controller =
-      ChatBottomPanelContainerController<PanelType>();
+  final controller = ChatBottomPanelContainerController<PanelType>();
 
   PanelType currentPanelType = PanelType.none;
 
