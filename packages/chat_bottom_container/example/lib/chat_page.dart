aa259cfcccf63fda05d0a433af6e80abdba1b90d
LinXunFeng <linxunfeng@yeah.net>
2024-06-29 15:15:33 +0800

    update(chat_bottom_container): example

---
+++ packages/chat_bottom_container/example/lib/chat_page.dart
@@ -0,0 +1,203 @@
+/*
+ * @Author: LinXunFeng linxunfeng@yeah.net
+ * @Repo: https://github.com/LinXunFeng/flutter_chat_packages
+ * @Date: 2024-06-29 11:55:20
+ */
+
+import 'package:chat_bottom_container/panel_container.dart';
+import 'package:flutter/material.dart';
+
+enum PanelType {
+  none,
+  keyboard,
+  emoji,
+  tool,
+}
+
+class ChatPage extends StatefulWidget {
+  const ChatPage({
+    super.key,
+    this.safeAreaBottom,
+    this.showAppBar = true,
+    this.changeKeyboardPanelHeight,
+  });
+
+  final double? safeAreaBottom;
+
+  final bool showAppBar;
+
+  final ChatKeyboardChangeKeyboardPanelHeight? changeKeyboardPanelHeight;
+
+  @override
+  State<ChatPage> createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
+  Color panelBgColor = const Color(0xFFF5F5F5);
+
+  final FocusNode inputFocusNode = FocusNode();
+
+  final controller = ChatBottomPanelContainerController<PanelType>();
+
+  PanelType currentPanelType = PanelType.none;
+
+  @override
+  void dispose() {
+    inputFocusNode.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: widget.showAppBar
+          ? AppBar(
+              title: const Text('Chat Page'),
+            )
+          : null,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(child: _buildList()),
+          _buildInputView(),
+          _buildPanelContainer(),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildList() {
+    Widget resultWidget = ListView.builder(
+      padding: EdgeInsets.zero,
+      reverse: true,
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: Text('Item $index'),
+        );
+      },
+    );
+    resultWidget = GestureDetector(
+      behavior: HitTestBehavior.translucent,
+      child: resultWidget,
+      onTapDown: (details) {
+        // Hide the panel when the ListView is touched.
+        hidePanel();
+      },
+    );
+    return resultWidget;
+  }
+
+  Widget _buildPanelContainer() {
+    return ChatBottomPanelContainer<PanelType>(
+      controller: controller,
+      inputFocusNode: inputFocusNode,
+      otherPanelWidget: (type) {
+        if (type == null) return const SizedBox.shrink();
+        switch (type) {
+          case PanelType.emoji:
+            return _buildEmojiPickerPanel();
+          case PanelType.tool:
+            return _buildToolPanel();
+          default:
+            return const SizedBox.shrink();
+        }
+      },
+      onPanelTypeChange: (panelType, data) {
+        switch (panelType) {
+          case ChatBottomPanelType.none:
+            currentPanelType = PanelType.none;
+            break;
+          case ChatBottomPanelType.keyboard:
+            currentPanelType = PanelType.keyboard;
+            break;
+          case ChatBottomPanelType.other:
+            if (data == null) return;
+            switch (data) {
+              case PanelType.emoji:
+                currentPanelType = PanelType.emoji;
+                break;
+              case PanelType.tool:
+                currentPanelType = PanelType.tool;
+                break;
+              default:
+                currentPanelType = PanelType.none;
+                break;
+            }
+            break;
+        }
+      },
+      changeKeyboardPanelHeight: widget.changeKeyboardPanelHeight,
+      panelBgColor: panelBgColor,
+      safeAreaBottom: widget.safeAreaBottom,
+    );
+  }
+
+  Widget _buildToolPanel() {
+    return Container(
+      height: 450,
+      color: Colors.red[50],
+      child: const Center(
+        child: Text('Tool Panel'),
+      ),
+    );
+  }
+
+  Widget _buildEmojiPickerPanel() {
+    return Container(
+      padding: EdgeInsets.zero,
+      height: 300,
+      color: Colors.blue[50],
+      child: const Center(
+        child: Text('Emoji Panel'),
+      ),
+    );
+  }
+
+  Widget _buildInputView() {
+    return Container(
+      height: 50,
+      color: Colors.white,
+      child: Row(
+        children: [
+          const SizedBox(width: 15),
+          Expanded(
+            child: TextField(
+              focusNode: inputFocusNode,
+            ),
+          ),
+          GestureDetector(
+            child: const Icon(Icons.emoji_emotions_outlined, size: 30),
+            onTap: () {
+              updatePanelType(PanelType.emoji);
+            },
+          ),
+          GestureDetector(
+            child: const Icon(Icons.add, size: 30),
+            onTap: () {
+              updatePanelType(PanelType.tool);
+            },
+          ),
+          const SizedBox(width: 15),
+        ],
+      ),
+    );
+  }
+
+  updatePanelType(PanelType type) {
+    controller.updatePanelType(
+      type == currentPanelType
+          ? ChatBottomPanelType.keyboard
+          : ChatBottomPanelType.other,
+      data: type,
+    );
+  }
+
+  hidePanel() {
+    if (inputFocusNode.hasFocus) {
+      inputFocusNode.unfocus();
+    }
+    if (ChatBottomPanelType.none == controller.currentPanelType) return;
+    controller.updatePanelType(ChatBottomPanelType.none);
+  }
+}
--- packages/chat_bottom_container/example/lib/main.dart
+++ packages/chat_bottom_container/example/lib/main.dart
@@ -4,7 +4,8 @@
  * @Date: 2024-06-15 17:48:05
  */
 
-import 'package:chat_bottom_container/chat_bottom_container.dart';
+import 'package:chat_bottom_container_example/chat_page.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
@@ -15,38 +16,37 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Home(),
+      home: HomePage(),
     );
   }
 }
 
-enum PanelType {
-  none,
-  keyboard,
-  emoji,
-  tool,
-}
-
-class Home extends StatefulWidget {
-  const Home({super.key});
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
   @override
-  State<Home> createState() => _HomeState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _HomeState extends State<Home> {
-  Color panelBgColor = const Color(0xFFF5F5F5);
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
 
-  final FocusNode inputFocusNode = FocusNode();
+  final bottomNavigationBarKey = GlobalKey();
 
-  ChatBottomPanelContainerController<PanelType> controller =
-      ChatBottomPanelContainerController<PanelType>();
+  @override
+  void initState() {
+    super.initState();
 
-  PanelType currentPanelType = PanelType.none;
+    _tabController = TabController(
+      length: 3,
+      vsync: this,
+    );
+  }
 
   @override
   void dispose() {
-    inputFocusNode.dispose();
+    _tabController.dispose();
     super.dispose();
   }
 
@@ -54,127 +54,113 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Expanded(
-            child: ListView.builder(
-              padding: EdgeInsets.zero,
-              reverse: true,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  title: Text('Item $index'),
+      appBar: AppBar(
+        title: const Text('Chat Bottom Container Example'),
+      ),
+      bottomNavigationBar: _buildBottomNavBar(),
+      body: _buildBody(),
+    );
+  }
+
+  Widget _buildBody() {
+    Widget resultWidget = TabBarView(
+      controller: _tabController,
+      children: [
+        ChatPage(
+          safeAreaBottom: 0,
+          showAppBar: false,
+          changeKeyboardPanelHeight: (keyboardHeight) {
+            // Here we need to subtract the height of BottomNavigationBar.
+            final renderObj =
+                bottomNavigationBarKey.currentContext?.findRenderObject();
+            if (renderObj is! RenderBox) return keyboardHeight;
+            return keyboardHeight - renderObj.size.height;
+          },
+        ),
+        Container(color: Colors.red),
+        Container(color: Colors.blue),
+      ],
+    );
+    resultWidget = Stack(
+      children: [
+        resultWidget,
+        Positioned(right: 10, child: _buildFloatingView()),
+      ],
+    );
+    return resultWidget;
+  }
+
+  Widget _buildBottomNavBar() {
+    return BottomNavigationBar(
+      key: bottomNavigationBarKey,
+      items: const [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.chat),
+          label: 'Chat',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.people),
+          label: 'People',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.settings),
+          label: 'Settings',
+        ),
+      ],
+    );
+  }
+
+  Widget _buildFloatingView() {
+    Widget resultWidget = Column(
+      children: [
+        _buildFloatingBtn(
+          icon: Icons.chevron_right_sharp,
+          onPressed: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) {
+                  return const ChatPage();
+                },
+              ),
+            );
+          },
+        ),
+        const SizedBox(height: 10),
+        _buildFloatingBtn(
+          icon: Icons.keyboard_arrow_up_sharp,
+          onPressed: () {
+            showModalBottomSheet(
+              context: context,
+              isScrollControlled: true,
+              builder: (context) {
+                return const FractionallySizedBox(
+                  heightFactor: 0.9,
+                  child: ChatPage(),
                 );
               },
-            ),
-          ),
-          _buildInputView(),
-          _buildPanelContainer(),
-        ],
+            );
+          },
+        ),
+      ],
+    );
+    return resultWidget;
+  }
+
+  Widget _buildFloatingBtn({
+    required void Function()? onPressed,
+    IconData? icon,
+  }) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        shadowColor: Colors.grey[50],
+        elevation: 2,
       ),
-    );
-  }
-
-  Widget _buildPanelContainer() {
-    return ChatBottomPanelContainer<PanelType>(
-      controller: controller,
-      inputFocusNode: inputFocusNode,
-      otherPanelWidget: (type) {
-        if (type == null) return const SizedBox.shrink();
-        switch (type) {
-          case PanelType.emoji:
-            return _buildEmojiPickerPanel();
-          case PanelType.tool:
-            return _buildToolPanel();
-          default:
-            return const SizedBox.shrink();
-        }
-      },
-      onPanelTypeChange: (panelType, data) {
-        switch (panelType) {
-          case ChatBottomPanelType.none:
-            currentPanelType = PanelType.none;
-            break;
-          case ChatBottomPanelType.keyboard:
-            currentPanelType = PanelType.keyboard;
-            break;
-          case ChatBottomPanelType.other:
-            if (data == null) return;
-            switch (data) {
-              case PanelType.emoji:
-                currentPanelType = PanelType.emoji;
-                break;
-              case PanelType.tool:
-                currentPanelType = PanelType.tool;
-                break;
-              default:
-                currentPanelType = PanelType.none;
-                break;
-            }
-            break;
-        }
-      },
-      panelBgColor: panelBgColor,
-    );
-  }
-
-  Widget _buildToolPanel() {
-    return Container(
-      height: 550,
-      color: Colors.red[50],
-      child: const Center(
-        child: Text('Tool Panel'),
-      ),
-    );
-  }
-
-  Widget _buildEmojiPickerPanel() {
-    return Container(
-      padding: EdgeInsets.zero,
-      height: 300,
-      color: Colors.blue[50],
-      child: const Center(
-        child: Text('Emoji Panel'),
-      ),
-    );
-  }
-
-  Widget _buildInputView() {
-    return Container(
-      height: 50,
-      color: Colors.white,
-      child: Row(
-        children: [
-          const SizedBox(width: 15),
-          Expanded(
-            child: TextField(
-              focusNode: inputFocusNode,
-            ),
-          ),
-          GestureDetector(
-            child: const Icon(Icons.emoji_emotions_outlined, size: 30),
-            onTap: () {
-              updatePanelType(PanelType.emoji);
-            },
-          ),
-          GestureDetector(
-            child: const Icon(Icons.add, size: 30),
-            onTap: () {
-              updatePanelType(PanelType.tool);
-            },
-          ),
-          const SizedBox(width: 15),
-        ],
-      ),
-    );
-  }
-
-  updatePanelType(PanelType type) {
-    controller.updatePanelType(
-      type == currentPanelType
-          ? ChatBottomPanelType.keyboard
-          : ChatBottomPanelType.other,
-      data: type,
+      child: Icon(icon),
     );
   }
 }
